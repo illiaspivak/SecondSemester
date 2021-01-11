2680b0ed351c934a367e0bceadb7cbf543d10c88
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-11 10:39:59 +0100

    Najviac delitelov

---
+++ src/sk/kosickaakademia/illiaspivak/rozcvicka/Main.java
@@ -0,0 +1,37 @@
+package sk.kosickaakademia.illiaspivak.rozcvicka;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter a number:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("najviac delitelov ma cislo: " + Deliteli(n));
+    }
+
+    /**
+     *Hľadáme čísla, ktoré majú čo najviac prirodzených deliteľov.
+     * Z čísel do 50 je to číslo 48, ktoré má 10 deliteľov.
+     * Ktoré z čísel menších ako 1000 má najviac deliteľov?
+     */
+    public static int Deliteli (int n){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for(int j = n; j>0; j--) {
+            for (int i = 1; i <= j; i++) {
+                if (j % i == 0) {
+                    a++;
+                }
+            }
+            if(a>b){
+                b=a;
+                c=j;
+            }
+            a=0;
+        }
+        return c;
+
+    }
+}
